35371d7e313fb7be406d29c57eca0b7d000c4103
denizcanilgin <39748708+denizcanilgin@users.noreply.github.com>
2019-09-06 22:36:52 +0300

    auth layout and activity added

---
+++ app/src/main/java/website/timrobinson/opencvtutorial/Auth.java
@@ -0,0 +1,15 @@
+package website.timrobinson.opencvtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class Auth extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+    }
+}
